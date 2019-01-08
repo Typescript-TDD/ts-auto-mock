f54c275efdf000d0e8ce57078d79dc3cf5f97ebd
uittorio <vittorio.gue@gmail.com>
2019-01-08 18:46:12 +0000

    add missing tests for dictionary

--- test/tranformer/descriptor/dictionary.test.ts
+++ test/tranformer/descriptor/dictionary.test.ts
@@ -12,5 +12,18 @@ describe('for dictionary', () => {
         const properties: Dictionary<Test> = createMock<Dictionary<Test>>();
         expect(properties).toEqual({});
     });
+    
+    describe('in a deep interface', () => {
+        interface InterfaceWithDictionary<T> {
+            a: string;
+            dictionary: Dictionary<T>
+        }
+        
+		it('should set an empty object', () => {
+			const properties: InterfaceWithDictionary<Test> = createMock<InterfaceWithDictionary<Test>>();
+			expect(properties.a).toBe("");
+			expect(properties.dictionary).toEqual({});
+		});
+    });
 });
 
