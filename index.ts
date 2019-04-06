4592cecba69ebfb271fd4aadcd7e16c6d0b0d246
Vittorio Guerriero <vittorio.gue@gmail.com>
2019-04-06 08:39:22 +0100

    fix #20

--- index.ts
+++ index.ts
@@ -4,4 +4,4 @@ export { MockMarker } from "./src/mockMarker/mockMarker";
 export { MockMethod } from "./src/mockMethod/mockMethod";
 export { On, AutoMockExtensionHandler } from "./src/framework/framework";
 export { createMock } from "./src/transformer/create-mock";
-export {method} from "./src/framework/methodExtension";
+export { method } from "./src/framework/methodExtension";
