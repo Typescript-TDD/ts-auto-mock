5b84a55803a6b2100af3cd1200bf0c6a8894d4b6
Vittorio Guerriero <vittorio.gue@gmail.com>
2019-04-07 11:55:57 +0100

    fix(tslinttest): fix another unit test

--- test/framework/primtive.test.ts
+++ test/framework/primtive.test.ts
@@ -1,18 +1,19 @@
-import { createMock, method, On } from "ts-auto-mock";
+import { createMock, method, On } from 'ts-auto-mock';
 
 describe('primitives', () => {
-	type a = string;
-	let mock: a;
-	
-	beforeEach(() => {
-		// @ts-ignore
-		mock = createMock<a>();
-	});
-	
-	it('should not be able to get the mock', () => {
-		expect(() => {
-			// @ts-ignore
-			On(mock).get(method(x => x.apply))
-		}).toThrow();
-	});
+    type a = string;
+    let mock: a;
+
+    beforeEach(() => {
+        // @ts-ignore
+        mock = createMock<a>();
+    });
+
+    it('should not be able to get the mock', () => {
+        expect(() => {
+            // tslint:disable
+            // @ts-ignore
+            On(mock).get(method((x) => x.apply));
+        }).toThrow();
+    });
 });
