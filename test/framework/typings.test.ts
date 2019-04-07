6721e565b4c5202b3e72ebe930982b1d93056b0e
Vittorio Guerriero <vittorio.gue@gmail.com>
2019-04-07 11:50:18 +0100

    fix one test lint

--- test/framework/typings.test.ts
+++ test/framework/typings.test.ts
@@ -1,22 +1,25 @@
-import {createMock, method, On} from "ts-auto-mock";
+import { createMock, method, On } from 'ts-auto-mock';
 
 describe('when creating a mock', () => {
     it('should compile when accessing to extended typings', () => {
         interface A<T> {
             a: T;
-            b: () => {}
+            b: () => {};
         }
 
         interface Interface {
+            arrayExpectGenerateMethod: number[];
+
             methodExpectJasmineExtension(): string;
-            arrayExpectGenerateMethod: Array<number>;
+
             methodReturnMockedType(): A<() => {}>;
         }
 
         const mock: Interface = createMock<Interface>();
-        const spy = On(mock).get(method(x => x.methodExpectJasmineExtension));
-        spy.and.returnValue("");
-        //mock.arrayExpectGenerateMethod.generateList(3); //TODO Implements functionality
-        //mock.methodReturnMockedType().a.and.returnValue(2);
+        //tslint:disable
+        const spy: jasmine.Spy = On(mock).get(method((x) => x.methodExpectJasmineExtension));
+        spy.and.returnValue('');
+        // mock.arrayExpectGenerateMethod.generateList(3); //TODO Implements functionality
+        // mock.methodReturnMockedType().a.and.returnValue(2);
     });
 });
