484388abf0a01f0b0c90edc55a77c760fd61959e
Vittorio Guerriero <vittorio.gue@gmail.com>
2019-04-07 11:02:00 +0100

    0.0.25

--- index.ts
+++ index.ts
@@ -5,4 +5,3 @@ export { MockMethod } from "./src/mockMethod/mockMethod";
 export { On, AutoMockExtensionHandler } from "./src/framework/framework";
 export { createMock } from "./src/transformer/create-mock";
 export { method } from "./src/framework/methodExtension";
-
