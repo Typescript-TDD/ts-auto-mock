75cbcf4de90c808290965169fed3cb62ffaadcb2
Vittorio Guerriero <vittorio.gue@gmail.com>
2019-11-04 17:42:38 +0000

    merge generic tests and add reuse test

--- test/transformer/descriptor/generic/reuse.test.ts
+++ test/transformer/descriptor/generic/reuse.test.ts
@@ -8,7 +8,9 @@ describe('for generic reuse', () => {
     it('should assign the correct value', () => {
         const mockString: Interface<string> = createMock<Interface<string>>();
         const mockNumber: Interface<number> = createMock<Interface<number>>();
+        const mockStringAgain: Interface<string> = createMock<Interface<string>>();
         expect(mockString.generic).toBe('');
         expect(mockNumber.generic).toBe(0);
+        expect(mockStringAgain.generic).toBe('');
     });
 });
