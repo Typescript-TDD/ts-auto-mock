164b6f4249599135173e473178ea88064fcdf79a
Vittorio Guerriero <vittorio.gue@gmail.com>
2019-11-16 11:13:26 +0000

    make sure variable declaration is ignored

--- src/transformer/descriptor/helper/helper.ts
+++ src/transformer/descriptor/helper/helper.ts
@@ -15,7 +15,7 @@ export namespace TypescriptHelper {
     export function GetDeclarationFromNode(node: ts.Node): ts.Declaration {
         const typeChecker: ts.TypeChecker = TypeChecker();
         const symbol: ts.Symbol = typeChecker.getSymbolAtLocation(node);
-        const declaration: ts.Declaration = symbol.declarations[0];
+        const declaration: ts.Declaration = GetFirstValidDeclaration(symbol.declarations);
 
         if (ts.isImportSpecifier(declaration)) {
             return GetDeclarationForImport(declaration) as ts.Declaration;
@@ -39,9 +39,9 @@ export namespace TypescriptHelper {
 
     export function GetDeclarationFromType(type: ts.Type): ts.TypeNode | ts.Declaration {
         if (type.symbol && type.symbol.declarations) {
-            return type.symbol.declarations[0];
+            return GetFirstValidDeclaration(type.symbol.declarations);
         } else if (type.aliasSymbol && type.aliasSymbol.declarations) {
-            return type.aliasSymbol.declarations[0];
+            return GetFirstValidDeclaration(type.aliasSymbol.declarations);
         }
 
         return TypeChecker().typeToTypeNode(type);
@@ -60,5 +60,11 @@ export namespace TypescriptHelper {
                 return current as ts.Declaration;
             }
         }
+    }
+
+    function GetFirstValidDeclaration(declarations: ts.Declaration[]): ts.Declaration {
+        return declarations.filter((declaration: ts.Declaration) => {
+            return !ts.isVariableDeclaration(declaration);
+        })[0];
     }
 }
