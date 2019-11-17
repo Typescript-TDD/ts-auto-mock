d620d053e3bfe66133a675e97f772da5f592fa78
Vittorio Guerriero <vittorio.gue@gmail.com>
2019-11-17 08:06:01 +0000

    remove unnecessary if statement

--- src/transformer/descriptor/properties/properties.ts
+++ src/transformer/descriptor/properties/properties.ts
@@ -5,10 +5,6 @@ import { TypeChecker } from '../../typeChecker/typeChecker';
 import { GetMockPropertiesFromSymbol } from '../mock/mockProperties';
 
 export function GetProperties(node: ts.Node, scope: Scope): ts.Expression {
-    if (ts.isInterfaceDeclaration(node) || ts.isClassDeclaration(node)) {
-       scope.declarationNode = node;
-    }
-
     const typeChecker: ts.TypeChecker = TypeChecker();
     const type: ts.Type = typeChecker.getTypeAtLocation(node);
     const symbols: ts.Symbol[] = typeChecker.getPropertiesOfType(type);
