d0269190fb6a8b845d373f242f80b6d28012ef6f
Vittorio <vittorio.gue@gmail.com>
2019-11-21 19:02:09 +0000

    chore(performance) improve array comparison performance

--- src/transformer/array/array.ts
+++ src/transformer/array/array.ts
@@ -1,19 +1,24 @@
 export namespace ArrayHelper {
     export function AreEqual<T>(array1: T[], array2: T[]): boolean {
-        if (array1.length !== array2.length) {
+        const array1Length: number = array1.length;
+
+        if (array1Length !== array2.length) {
             return false;
         }
 
-        const array2Copy: T[] = [...array2];
-
-        array1.forEach((item: T) => {
-            const index: number = array2Copy.indexOf(item);
-            if (index >= 0) {
-                array2Copy.splice(index, 1);
+        for (let i: number = 0; i < array1Length; i++) {
+            if (array2.indexOf(array1[i]) === -1) {
+                return false;
             }
-        });
+        }
 
-        return array2Copy.length === 0;
+        for (let i: number = 0; i < array1Length; i++) {
+            if (array1.indexOf(array2[i]) === -1) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     export function ArrayFromLength(length: number): Array<number> {
