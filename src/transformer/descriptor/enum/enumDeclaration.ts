46425201ec35510e78eeea8d8d14951195b693c3
Vittorio <vittorio.gue@gmail.com>
2019-11-30 10:19:58 +0000

    fix(enum): add module declaration enum support

--- src/transformer/descriptor/enum/enumDeclaration.ts
+++ src/transformer/descriptor/enum/enumDeclaration.ts
@@ -5,5 +5,9 @@ export function GetEnumDeclarationDescriptor(node: ts.EnumDeclaration): ts.Expre
     const typeChecker: ts.TypeChecker = TypeChecker();
     const type: ts.LiteralType = typeChecker.getTypeAtLocation(node.members[0]) as ts.LiteralType;
 
-    return ts.createLiteral(type.value);
+    if (type.hasOwnProperty('value')) {
+        return ts.createLiteral(type.value);
+    }
+
+    return ts.createLiteral(0);
 }
