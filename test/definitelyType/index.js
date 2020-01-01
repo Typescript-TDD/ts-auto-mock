175b2a7b898936eac17919f1c9043d06e7616efe
Pmyl <julo134@gmail.com>
2020-01-01 16:22:55 +0000

    maximise types per process, add ability to specify to run all types

--- test/definitelyType/index.js
+++ test/definitelyType/index.js
@@ -2,17 +2,62 @@ const exec = require('child_process').exec;
 const path = require('path');
 const fs = require('fs');
 
-const typesCountPerProcess = process.argv[2] && parseInt(process.argv[2]) || 50;
-const processesCount = process.argv[3] && parseInt(process.argv[3]) || 1;
-
 const getDirectories = source =>
   fs.readdirSync(source, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name);
 
-
 const folder = path.join("DefinitelyTyped", "types");
+
 const definitelyTypedDirectories = getDirectories(folder);
+const processesConfig = getProcessesConfig();
+
+function getProcessesCount() {
+    return process.argv[3] && parseInt(process.argv[3]) || 1;
+}
+
+function getProcessesConfig() {
+    const totalTypesCount = getTotalTypesCount();
+    const processesMaximized = maximiseTypesInProcesses(getProcessesCount(), totalTypesCount);
+    const sum = processesMaximized.reduce((previous, current) => previous + current.typesCount, 0);
+    const avg = sum / processesMaximized.length;
+
+    return {
+        totalTypesCount: totalTypesCount,
+        processes: processesMaximized,
+        averageTypesCountPerProcess: avg
+    };
+}
+
+function maximiseTypesInProcesses(processesCount, totalTypesCount) {
+    const processes = [];
+    const floored = Math.floor(totalTypesCount / processesCount);
+    let remaining = totalTypesCount;
+
+    for(let i = 0; i < processesCount; i++) {
+        processes.push({
+            typesCount: Math.min(floored, remaining)
+        });
+
+        remaining -= floored;
+    }
+
+    return processes;
+}
+
+function getTotalTypesCount() {
+    if (process.argv[2]) {
+        const maybeCount = parseInt(process.argv[2]);
+
+        if (!Number.isNaN(maybeCount)) {
+            return Math.min(definitelyTypedDirectories.length, maybeCount);
+        } else if (process.argv[2] === "all") {
+            return definitelyTypedDirectories.length;
+        }
+    }
+    
+    return 50;
+}
 
 function runAllDir(dirs, id) {
     fs.writeFileSync(`tsLogs.${id}.txt`, "===== Start run " + new Date().toISOString() + " =====\n", { flag: "a+" });
@@ -28,33 +73,42 @@ function runAllDir(dirs, id) {
 
 let startDirectoryIndex = 0;
 
-console.log(`Processes: ${processesCount}`);
-console.log(`Types per process: ${typesCountPerProcess}`);
-console.log(`Total: ${Math.min(definitelyTypedDirectories.length, processesCount * typesCountPerProcess)}\n`);
+console.log(`Total types: ${processesConfig.totalTypesCount}`);
+console.log(`Processes: ${processesConfig.processes.length}`);
+console.log(`Average types per process: ${processesConfig.averageTypesCountPerProcess}`);
+console.log();
 
 runApp();
 
 async function runApp() {
     await installDependencies();
 
-    for(let i = 0; i < processesCount; i++) {
-        runAllDir(definitelyTypedDirectories.slice(startDirectoryIndex, startDirectoryIndex + typesCountPerProcess), i);
-        startDirectoryIndex += typesCountPerProcess;
+    for(let i = 0; i < processesConfig.processes.length; i++) {
+        runAllDir(definitelyTypedDirectories.slice(startDirectoryIndex, startDirectoryIndex + processesConfig.processes[i].typesCount), i);
+        startDirectoryIndex += processesConfig.processes[i].typesCount;
     }
 }
 
 async function installDependencies() {
-    const processedDirectories = definitelyTypedDirectories.slice(0, typesCountPerProcess * processesCount);
+    const processedDirectories = definitelyTypedDirectories.slice(0, processesConfig.totalTypesCount);
+
+    const directoriesWithDependencies = processedDirectories
+        .map(dir => ({ name: dir, path: path.join(folder, dir) }))
+        .filter(dir => fs.existsSync(path.join(dir.path, "package.json")));
+    
+    if (directoriesWithDependencies.length === 0) {
+        return Promise.resolve();
+    }
+
+    console.log(`npm install in:`);
 
     return Promise.all(
-        processedDirectories
-            .map(dir => path.join(folder, dir))
-            .filter(directoryPath => fs.existsSync(path.join(directoryPath, "package.json")))
-            .map(directoryPath => {
-                console.log(`Executing \`npm --prefix ${directoryPath} install ${directoryPath}\``);
-                return execPromise(`npm --prefix ${directoryPath} install ${directoryPath}`).catch(e => e).then(r => console.log(r));
+        directoriesWithDependencies
+            .map(dir => {
+                process.stdout.write(`${dir.name} `);
+                return execPromise(`npm --prefix ${dir.path} install ${dir.path}`);
             })
-    );
+    ).then(() => console.log("\n"));
 }
 
 async function run(dir, id) {
@@ -77,21 +131,19 @@ async function run(dir, id) {
         .then((response) => {
             if (response) {
                 process.stdout.write(`TYPE: ${dir} P${id} `);
-                console.warn("O");
+                console.warn("☐");
                 fs.writeFileSync(`tsLogs.${id}.txt`, "Success with response in " + dir + ':\n' + response, { flag:'a+' });
             } else {
                 process.stdout.write(`TYPE: ${dir} P${id} `);
-                console.info("Y");
+                console.info("✔");
             }
         })
         .catch(error => {
             process.stdout.write(`TYPE: ${dir} P${id} `);
-            console.error("X");
+            console.error("✘");
             fs.writeFileSync(`tsLogs.${id}.txt`, "Error in " + dir + ':\n' + error, { flag:'a+' });
         });
 }
-
-
 
 function execPromise(command) {
     return new Promise(function(resolve, reject) {
