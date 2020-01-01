0974d373699e4eea31ce368a98b398b0962369bd
Vittorio Guerriero <vittorio.gue@gmail.com>
2020-01-01 18:23:14 +0000

    updates

--- performance/core/git/gitHelper.js
+++ performance/core/git/gitHelper.js
@@ -3,7 +3,7 @@ const execPromise = require('../exec/execPromise');
 function gitHelper() {
     return {
         getCurrentBranchName() {
-            return execPromise(`git for-each-ref --format='%(objectname) %(refname:short)' refs/heads | awk "/^$(git rev-parse HEAD)/ {print \$2}"`);
+            return execPromise(`git ls-remote --heads origin | grep $(git rev-parse HEAD) | cut -d / -f 3`);
         },
         getCurrentCommit() {
             return execPromise('git rev-parse HEAD\n');
