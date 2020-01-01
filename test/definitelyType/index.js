85430d8b6dfd17501f2d18fa91bac81eb2bd265c
Pmyl <julo134@gmail.com>
2020-01-01 15:48:14 +0000

    add some logs, install dependencies of types with package.json

--- test/definitelyType/index.js
+++ test/definitelyType/index.js
@@ -2,6 +2,9 @@ const exec = require('child_process').exec;
 const path = require('path');
 const fs = require('fs');
 
+const typesCountPerProcess = process.argv[2] && parseInt(process.argv[2]) || 50;
+const processesCount = process.argv[3] && parseInt(process.argv[3]) || 1;
+
 const getDirectories = source =>
   fs.readdirSync(source, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
@@ -11,19 +14,50 @@ const getDirectories = source =>
 const folder = path.join("DefinitelyTyped", "types");
 const definitelyTypedDirectories = getDirectories(folder);
 
-function runAllDir(dirs) {
-    return dirs.reduce(
-      (promise, dir) => {
-          return promise.then(() => run(dir)).catch(() => Promise.resolve());
-      },
-      Promise.resolve()
+function runAllDir(dirs, id) {
+    fs.writeFileSync(`tsLogs.${id}.txt`, "===== Start run " + new Date().toISOString() + " =====\n", { flag: "a+" });
+    fs.writeFileSync(`tsconfig.types.${id}.json`, "");
+    
+    return dirs.reduce((promise, dir) => promise.then(() => run(dir, id)), Promise.resolve())
+        .then(() => {
+            fs.unlinkSync(`tsconfig.types.${id}.json`);
+        });
+}
+
+// console.log(definitelyTypedDirectories);
+
+let startDirectoryIndex = 0;
+
+console.log(`Processes: ${processesCount}`);
+console.log(`Types per process: ${typesCountPerProcess}`);
+console.log(`Total: ${Math.min(definitelyTypedDirectories.length, processesCount * typesCountPerProcess)}\n`);
+
+runApp();
+
+async function runApp() {
+    await installDependencies();
+
+    for(let i = 0; i < processesCount; i++) {
+        runAllDir(definitelyTypedDirectories.slice(startDirectoryIndex, startDirectoryIndex + typesCountPerProcess), i);
+        startDirectoryIndex += typesCountPerProcess;
+    }
+}
+
+async function installDependencies() {
+    const processedDirectories = definitelyTypedDirectories.slice(0, typesCountPerProcess * processesCount);
+
+    return Promise.all(
+        processedDirectories
+            .map(dir => path.join(folder, dir))
+            .filter(directoryPath => fs.existsSync(path.join(directoryPath, "package.json")))
+            .map(directoryPath => {
+                console.log(`Executing \`npm --prefix ${directoryPath} install ${directoryPath}\``);
+                return execPromise(`npm --prefix ${directoryPath} install ${directoryPath}`).catch(e => e).then(r => console.log(r));
+            })
     );
 }
 
-console.log(definitelyTypedDirectories);
-runAllDir([definitelyTypedDirectories[0]]);
-
-async function run(dir) {
+async function run(dir, id) {
     const config = {
         "extends": `./DefinitelyTyped/types/${dir}/tsconfig.json`,
         "compilerOptions": {
@@ -37,26 +71,37 @@ async function run(dir) {
         }
     };
 
-    await fs.writeFileSync('tsconfig.types.json', JSON.stringify(config));
-    console.log(`TYPE: ${dir}`);
+    fs.writeFileSync(`tsconfig.types.${id}.json`, JSON.stringify(config));
 
-    return execPromise('npx ttsc --project tsconfig.types.json');
+    return execPromise(`npx ttsc --project tsconfig.types.${id}.json`)
+        .then((response) => {
+            if (response) {
+                process.stdout.write(`TYPE: ${dir} P${id} `);
+                console.warn("O");
+                fs.writeFileSync(`tsLogs.${id}.txt`, "Success with response in " + dir + ':\n' + response, { flag:'a+' });
+            } else {
+                process.stdout.write(`TYPE: ${dir} P${id} `);
+                console.info("Y");
+            }
+        })
+        .catch(error => {
+            process.stdout.write(`TYPE: ${dir} P${id} `);
+            console.error("X");
+            fs.writeFileSync(`tsLogs.${id}.txt`, "Error in " + dir + ':\n' + error, { flag:'a+' });
+        });
 }
+
 
 
 function execPromise(command) {
     return new Promise(function(resolve, reject) {
         exec(command, (error, stdout) => {
             if (error) {
-                fs.writeFileSync("tsLogs.txt", '\n' + error, { flag:'a+' });
                 reject(error);
                 return;
             }
 
-            const response = stdout.trim();
-            console.log(response);
-            fs.writeFileSync("tsLogs.txt", '\n' + response, { flag:'a+' });
-            resolve(response);
+            resolve(stdout.trim());
         });
     });
 }
