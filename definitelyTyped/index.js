ed784834033b7304494d0887cc531af302630932
Pmyl <julo134@gmail.com>
2020-01-09 13:02:47 +0000

    put back on push hook for testing

--- definitelyTyped/index.js
+++ definitelyTyped/index.js
@@ -4,7 +4,6 @@ process.on('unhandledRejection', error => {
 });
 
 require('dotenv').config();
-
 const path = require('path');
 const fs = require('fs');
 const processService = require('../utils/process/process')(process);
