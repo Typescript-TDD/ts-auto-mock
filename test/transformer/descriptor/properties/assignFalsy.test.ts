412fd4effba2b877f7b23e9b4c116c753b34698c
Pmyl <julo134@gmail.com>
2020-01-30 19:46:40 +0000

    fix(mockProperty): make sure a falsy value can be assigned to a mock property

---
+++ test/transformer/descriptor/properties/assignFalsy.test.ts
@@ -0,0 +1,49 @@
+import { createMock } from 'ts-auto-mock';
+
+describe('mocking properties', () => {
+  describe('when property is a mock', () => {
+    interface SubInterface {
+      aProp: string;
+    }
+
+    interface AnInterface {
+      bProp: SubInterface;
+    }
+
+    it('should correctly assign null', () => {
+      const anInterface: AnInterface = createMock<AnInterface>();
+
+      anInterface.bProp = null;
+
+      expect(anInterface.bProp).toBeNull();
+    });
+  });
+
+  describe('when property is a value type', () => {
+    interface AnInterface {
+      aProp: string;
+    }
+
+    it('should correctly assign null', () => {
+      const anInterface: AnInterface = createMock<AnInterface>();
+
+      anInterface.aProp = null;
+
+      expect(anInterface.aProp).toBeNull();
+    });
+  });
+
+  describe('when property is a function', () => {
+    interface AnInterface {
+      aProp(): string;
+    }
+
+    it('should correctly assign null', () => {
+      const anInterface: AnInterface = createMock<AnInterface>();
+
+      anInterface.aProp = null;
+
+      expect(anInterface.aProp).toBeNull();
+    });
+  });
+});
