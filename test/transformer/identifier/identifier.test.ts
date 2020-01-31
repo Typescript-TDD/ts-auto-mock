4301c186734ec0aca663bd6036f2e3f85ea9012d
Vittorio <vittorio.gue@gmail.com>
2020-01-31 18:51:06 +0000

    fix/private-identifier make sure import module variable are unique (#211)

---
+++ test/transformer/identifier/identifier.test.ts
@@ -0,0 +1,41 @@
+import { createMock } from 'ts-auto-mock';
+
+describe('when variables are created in a for', () => {
+  it('should not have conflicts with ts auto mock imports', () => {
+    interface MyInterface {
+      a: string;
+    }
+
+    createMock<MyInterface>();
+    const a: string[] = ['hello', 'world'];
+
+    expect(() => {
+      // eslint-disable-next-line no-empty
+      for (const c of a) {
+      }
+
+      // eslint-disable-next-line no-empty
+      for (const d of a) {
+
+      }
+
+      // eslint-disable-next-line no-empty
+      for (const e of a) {
+      }
+    }).not.toThrow();
+  });
+});
+
+describe('when there are variables with the same name as ts auto mock private identifiers', () => {
+  it('should have conflicts with ts auto mock imports', () => {
+    // eslint-disable-next-line @typescript-eslint/typedef
+    const ɵRepository = 'test';
+    interface MyInterface {
+      a: string;
+    }
+
+    expect(() => {
+      const myInstance: MyInterface = createMock<MyInterface>();
+    }).toThrow();
+  });
+});
