805debd1dbcc63789358141bb46603fa04b881f6
uittorio <vittorio.gue@gmail.com>
2020-02-03 19:12:51 +0000

    docs(website): add google analytics

--- ui/gatsby-config.js
+++ ui/gatsby-config.js
@@ -1,6 +1,13 @@
 module.exports = {
     plugins: [
         `gatsby-plugin-sass`,
-        `gatsby-plugin-offline`
+        `gatsby-plugin-offline`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-37077972-4",
+                head: false
+            },
+        }
     ]
 };
