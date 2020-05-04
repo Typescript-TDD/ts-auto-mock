00d256f051f253d2d96387abcf68d3815d8f6191
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2020-05-04 21:15:45 +0200

    fix(transformer): Revert to terminating early if a function declares its return type

--- src/transformer/descriptor/method/functionReturnType.ts
+++ src/transformer/descriptor/method/functionReturnType.ts
@@ -1,16 +1,36 @@
 import * as ts from 'typescript';
 import { GetReturnNodeFromBody } from './bodyReturnType';
 
+function isFunctionLikeDeclaration(node: ts.SignatureDeclaration): node is ts.FunctionLikeDeclaration {
+  switch (true) {
+    case ts.isFunctionDeclaration(node):
+    case ts.isMethodDeclaration(node):
+    case ts.isGetAccessorDeclaration(node):
+    case ts.isSetAccessorDeclaration(node):
+    case ts.isConstructorDeclaration(node):
+    case ts.isFunctionExpression(node):
+    case ts.isArrowFunction(node):
+      return true;
+  }
+
+  return false;
+}
+
 export function GetFunctionReturnType(node: ts.SignatureDeclaration): ts.Node {
-  if (ts.isArrowFunction(node) || ts.isFunctionExpression(node)) {
+  const declaredReturnType: ts.TypeNode | undefined = node.type;
+  if (declaredReturnType) {
+    return declaredReturnType;
+  }
+
+  if (isFunctionLikeDeclaration(node)) {
     return GetReturnNodeFromBody(node);
   }
 
-  if (!node.type) {
+  if (!declaredReturnType) {
     throw new Error(
       `The transformer couldn't determine the type of ${node.getText()}. Did you declare its type?`,
     );
   }
 
-  return node.type;
+  return declaredReturnType;
 }
