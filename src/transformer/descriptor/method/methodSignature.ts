0a2d62251225eb60b03eb35ba24b75a5c2fd8b40
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2020-05-17 13:57:00 +0200

    chore(transformer): Removed unused imports

--- src/transformer/descriptor/method/methodSignature.ts
+++ src/transformer/descriptor/method/methodSignature.ts
@@ -1,8 +1,6 @@
 import * as ts from 'typescript';
 import { Scope } from '../../scope/scope';
-import { GetDescriptor } from '../descriptor';
 import { TypescriptCreator } from '../../helper/creator';
-import { GetNullDescriptor } from '../null/null';
 import { GetMethodDescriptor } from './method';
 
 export function GetMethodSignatureDescriptor(node: ts.MethodSignature, scope: Scope): ts.Expression {
