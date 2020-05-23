0c4d7e42141cfe21148be093c2afa3f53dfac9f3
Vittorio <vittorio.gue@gmail.com>
2020-05-23 16:38:51 +0100

    feat(random): generate random string

--- src/transformer/descriptor/string/string.ts
+++ src/transformer/descriptor/string/string.ts
@@ -1,5 +1,9 @@
 import * as ts from 'typescript';
+import { IsTsAutoMockRandomEnabled } from '../../../options/random';
 
 export function GetStringDescriptor(): ts.Expression {
+  if (IsTsAutoMockRandomEnabled()) {
+    return ts.createLiteral(Math.random().toString(20).substr(2, 6));
+  }
   return ts.createLiteral('');
 }
