fd6ab0acf59153b6da79c837edf3131ab9492523
Vittorio <vittorio.gue@gmail.com>
2020-05-24 11:36:14 +0100

    use existing propertyName string getter

--- src/transformer/descriptor/string/string.ts
+++ src/transformer/descriptor/string/string.ts
@@ -2,11 +2,12 @@ import * as ts from 'typescript';
 import { IsTsAutoMockRandomEnabled } from '../../../options/random';
 import { PropertySignatureCache } from '../property/cache';
 import { RandomPropertyAccessor } from '../random/random';
+import { TypescriptHelper } from '../helper/helper';
 
 export function GetStringDescriptor(): ts.Expression {
   if (IsTsAutoMockRandomEnabled()) {
-    const propertyName: ts.PropertyName = PropertySignatureCache.instance.get();
-    const prefix: ts.StringLiteral = ts.createLiteral(`${(propertyName as ts.Identifier).escapedText}`);
+    const propertyName: string = TypescriptHelper.GetStringPropertyName(PropertySignatureCache.instance.get());
+    const prefix: ts.StringLiteral = ts.createLiteral(propertyName);
     return ts.createCall(RandomPropertyAccessor('string'), [], [prefix]);
   }
   return ts.createLiteral('');
