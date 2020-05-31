ec7a59960943958e28f1ba86fc328356ad5eca52
Geoffrey Testelin <geoffrey.testelin@gmail.com>
2020-05-31 10:27:01 +0200

    build(release): configure semantic release and commitizen (#342)
    * chore(npm): remove all scripts to create a release
    
    * chore(readme): add semantic release badge
    
    * build(dependencies): add new dependencies for semantic release
    
    * chore(release): configure semantic release behavior
    
    * chore(cz): configure cz to works with commitizen
    
    * chore(script): add new npm script to create a release
    
    * chore(scripts): add new npm scripts to run cz and the git hook
    
    * chore(commitizen): configure commitizen
    
    * build(dependencies): add cz-customizable-ghooks
    
    * chore(readme): add commitizen friendly badge
    
    * chore(scripts): remove npm prefix since we use local packages
    
    * build(release): rewrite the GitHub release workflow
    
    * build(semantic): add a configuration file for the semantic GH action
    
    * chore(semantic-release): enable the CI option
    
    * build(semantic): less hitler rules for the commits
    
    * chore(release): change the configuration
    
    use typescripttdd as owner
    remove the angular preset since the configuration is provided by commitizen
    
    * chore(commitizen): add the "ci" target
    
    * build(dependencies): remove useless dependencies
    
    * chore(commitizen): remove breaking changes for the build commit type
    
    * chore(release): use the angular preset
    
    * chore(release): use the angular preset
    
    * build(dependencies): add conventional-changelog-angular
    
    my bad, I used conventional-changelog instead of conventional-changelog-angular
    
    Co-authored-by: Vittorio Guerriero <vittorio.gue@gmail.com>

---
+++ commitizen.js
@@ -0,0 +1,59 @@
+module.exports = {
+  allowBreakingChanges: [
+    `feat`,
+    `fix`
+  ],
+  allowCustomScopes: true,
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  messages: {
+    body: `Provide a LONGER description of the change (optional). Use "|" to break new line:\n`,
+    breaking: `List any BREAKING CHANGES (optional):\n`,
+    confirmCommit: `Are you sure you want to proceed with the commit above ?`,
+    customScope: `SCOPE of this change:`,
+    scope: `\nSCOPE of this change (optional):`,
+    subject: `Write a SHORT, IMPERATIVE tense subject of the change:\n`,
+    type: `Select the type of change that you are committing:`
+  },
+  skipQuestions: [ `footer` ],
+  subjectLimit: 144,
+  // Any changes should also me made on the [semantic config](#/.github/semantic.yml)
+  types: [
+    {
+      name: `feat:     A new feature`,
+      value: `feat`
+    },
+    {
+      name: `fix:      A bug fix`,
+      value: `fix`
+    },
+    {
+      name: `style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)`,
+      value: `style`
+    },
+    {
+      name: `refactor: A code change that neither fixes a bug nor adds a feature`,
+      value: `refactor`
+    },
+    {
+      name: `perf:     A code change that improves performance`,
+      value: `perf`
+    },
+    {
+      name: `test:     Adding missing tests or correcting existing tests`,
+      value: `test`
+    },
+    {
+      name: `build:    Changes that affect the build system or external dependencies`,
+      value: `build`
+    },
+    {
+      name: `ci:       Changes that affect the CI configuration`,
+      value: `ci`
+    },
+    {
+      name: `chore:    Anything else`,
+      value: `chore`
+    }
+  ]
+};
