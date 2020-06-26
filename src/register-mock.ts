16506e5ae72d70dcf5c09c8881fe5b1bd71ce2e9
Pmyl <julo134@gmail.com>
2020-06-26 21:13:02 +0100

    feat(transformer): error when running tests with no transformer installation

--- src/register-mock.ts
+++ src/register-mock.ts
@@ -1,1 +1,7 @@
-export declare function registerMock<T extends object>(factory: () => T): void;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function registerMock<T extends object>(factory: () => T): void {
+  throw new Error(`
+    ts-auto-mock is not been configured as a transformer. Please follow the instructions in https://typescript-tdd.github.io/ts-auto-mock/installation
+    If help is required you can find us on https://typescript-tdd.github.io
+  `);
+}
