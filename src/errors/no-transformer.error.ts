dd73b57454a2c41e93e39a15b0057c9e0344edeb
Pmyl <julo134@gmail.com>
2020-06-27 17:43:50 +0100

    feat(transformer): extract repeated error in a constant, fixed error wording

---
+++ src/errors/no-transformer.error.ts
@@ -0,0 +1,4 @@
+export const NoTransformerError: string = `
+  hey, it looks like ts-auto-mock is not configured correctly! You can find the instructions here https://typescript-tdd.github.io/ts-auto-mock/installation.
+  If you need further assistance feel free to drop a message on slack. (link at the bottom of https://typescript-tdd.github.io/ts-auto-mock)
+`;
--- src/register-mock.ts
+++ src/register-mock.ts
@@ -1,7 +1,6 @@
+import { NoTransformerError } from './errors/no-transformer.error';
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function registerMock<T extends object>(factory: () => T): void {
-  throw new Error(`
-    ts-auto-mock is not been configured as a transformer. Please follow the instructions in https://typescript-tdd.github.io/ts-auto-mock/installation
-    If help is required you can find us on https://typescript-tdd.github.io
-  `);
+  throw new Error(NoTransformerError);
 }
