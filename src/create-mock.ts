717a885579eed04a8c04c3d6a37efffc1174e99e
Pmyl <julo134@gmail.com>
2020-06-27 17:43:50 +0100

    feat(transformer): extract repeated error in a constant, fixed error wording

--- src/create-mock.ts
+++ src/create-mock.ts
@@ -1,9 +1,7 @@
+import { NoTransformerError } from './errors/no-transformer.error';
 import { DeepPartial } from './partial/deepPartial';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function createMock<T extends object>(values?: DeepPartial<T>): T {
-  throw new Error(`
-    ts-auto-mock is not been configured as a transformer. Please follow the instructions in https://typescript-tdd.github.io/ts-auto-mock/installation
-    If help is required you can find us on https://typescript-tdd.github.io
-  `);
+  throw new Error(NoTransformerError);
 }
