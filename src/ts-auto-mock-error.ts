df888b277db0baba98384b1f76b66365480aefbb
Vittorio <vittorio.gue@gmail.com>
2020-06-28 09:06:10 +0100

    test commit

---
+++ src/ts-auto-mock-error.ts
@@ -0,0 +1,4 @@
+export const error: Error = new Error(`
+  ts-auto-mock is not been configured as a transformer. Please follow the instructions in https://typescript-tdd.github.io/ts-auto-mock/installation
+  If help is required you can find us on https://typescript-tdd.github.io
+`);
