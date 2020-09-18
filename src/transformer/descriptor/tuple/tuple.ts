60eaaa6a9432aa17dc9bbcbddeb12e9a68b16bda
Pmyl <julo134@gmail.com>
2020-09-18 13:23:45 +0100

    fix(typescript): restore support for typescript 3

--- src/transformer/descriptor/tuple/tuple.ts
+++ src/transformer/descriptor/tuple/tuple.ts
@@ -7,6 +7,21 @@ export function GetTupleDescriptor(
   scope: Scope
 ): ts.Expression {
   return ts.createArrayLiteral(
-    node.elements.map((e: ts.TypeNode) => GetDescriptor(e, scope))
+    getDescriptorFromTypeNodes(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ((node as any).elementTypes as ts.NodeArray<ts.TypeNode>) ||
+        // Typescript 4 doesn't have elementTypes but it does have elements, to support both versions
+        // this casting is needed.
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        ((node as any).elements as ts.NodeArray<ts.TypeNode>),
+      scope
+    )
   );
+}
+
+function getDescriptorFromTypeNodes(
+  nodes: ts.NodeArray<ts.TypeNode>,
+  scope: Scope
+): ts.Expression[] {
+  return nodes.map((e: ts.TypeNode) => GetDescriptor(e, scope));
 }
