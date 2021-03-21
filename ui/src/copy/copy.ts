d4ae7cb3866255b613f99841f19921e3643d90e4
Vittorio Guerriero <vittorio.gue@gmail.com>
2021-03-21 11:43:33 +0000

    chore(documentation): use correct quote (#632)

--- ui/src/copy/copy.ts
+++ ui/src/copy/copy.ts
@@ -1,20 +1,23 @@
 export const TsAutoMockCopy = {
-  title: "TypeScript transformer to create mocks for any type",
-  homeButton: "Get Started",
-  apiButton: "API Documentation",
-  slack: "Need help? Join us on Slack",
+  title: 'TypeScript transformer to create mocks for any type',
+  homeButton: 'Get Started',
+  apiButton: 'API Documentation',
+  slack: 'Need help? Join us on Slack',
   features: {
     real: {
-      title: "Real Mocks",
-      description: "The creation of mocks is done during TypeScript compilation and preserves all type information."
+      title: 'Real Mocks',
+      description:
+        'The creation of mocks is done during TypeScript compilation and preserves all type information.',
     },
     extendible: {
-      title: "Extendible",
-      description: "This library can be used with any testing framework. You can even provide your own implementation!"
+      title: 'Extendible',
+      description:
+        'This library can be used with any testing framework. You can even provide your own implementation!',
     },
     typeSafety: {
-      title: "Type Safety",
-      description: "No more casting to `any' to get your spy or mock. This library provides a mechanism to inject your spies while preserving their types."
-    }
-  }
-}
+      title: 'Type Safety',
+      description:
+        "No more casting to 'any' to get your spy or mock. This library provides a mechanism to inject your spies while preserving their types.",
+    },
+  },
+};
