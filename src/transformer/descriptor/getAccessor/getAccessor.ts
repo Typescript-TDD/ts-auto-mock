f835d85e7ae2aa5854a457da24c7c032873e75e5
Pmyl <julo134@gmail.com>
2021-05-19 10:33:25 +0100

    add name in property cache, add remaining tests, remove extra check

--- src/transformer/descriptor/getAccessor/getAccessor.ts
+++ src/transformer/descriptor/getAccessor/getAccessor.ts
@@ -2,19 +2,14 @@ import * as ts from 'typescript';
 import { Scope } from '../../scope/scope';
 import { GetDescriptor } from '../descriptor';
 import { GetFunctionReturnType } from '../method/functionReturnType';
+import { PropertySignatureCache } from '../property/cache';
 
 export function GetGetAccessorDeclarationDescriptor(
   node: ts.GetAccessorDeclaration,
   scope: Scope
 ): ts.Expression {
+  PropertySignatureCache.instance.set(node.name);
   const returnTypeNode: ts.Node = GetFunctionReturnType(node);
-  const returnType: ts.Expression = GetDescriptor(returnTypeNode, scope);
 
-  if (!node.name) {
-    throw new Error(
-      `The transformer couldn't determine the name of ${node.getText()}. Please report this incident.`
-    );
-  }
-
-  return returnType;
+  return GetDescriptor(returnTypeNode, scope);
 }
--- test/transformer/descriptor/getAccessor/getAccessor.test.ts
+++ test/transformer/descriptor/getAccessor/getAccessor.test.ts
@@ -13,12 +13,22 @@ describe('getAccessor', () => {
     public get prop3(): AClass {
       return new AClass();
     }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    public get prop4() {
+      return 123;
+    }
   }
 
-  it('should mock the correct type', () => {
+  it('should mock the correct explicit type', () => {
     const mock: AClass = createMock<AClass>();
     expect(mock.prop1).toBe(0);
     expect(mock.prop2).toBe('');
     expect(mock.prop3.prop3.prop3.prop1).toBe(0);
+  });
+
+  it('should mock the correct type inferred from the body', () => {
+    const mock: AClass = createMock<AClass>();
+    expect(mock.prop4).toBe(123);
   });
 });
