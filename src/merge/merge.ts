31c5ff5f09d59b11ada77e238ab70137da31c9cd
Giulio Caprino <julo134@gmail.com>
2021-05-22 10:53:41 +0100

    fix(merge): ensure undefined is assigned when merging partial mock (#748)
    Co-authored-by: Vittorio Guerriero <vittorio.gue@gmail.com>

--- src/merge/merge.ts
+++ src/merge/merge.ts
@@ -1,16 +1,33 @@
-import { mergeWith, isObject } from 'lodash-es';
+import { mergeWith, isObject, set } from 'lodash-es';
 import { ɵMarker as Marker } from 'ts-auto-mock/extension';
 import { PartialDeep } from '../partial/partial';
 
 export class Merge {
   public static merge<T>(a: T, b: PartialDeep<T>): T {
-    return mergeWith(a, b, (objValue: unknown, srcValue: unknown) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      if (isObject(srcValue) && srcValue[Marker.instance.get()]) {
-        return srcValue;
+    return mergeWith(
+      a,
+      b,
+      (
+        objValue: unknown,
+        srcValue: unknown,
+        oneLevelPath: string,
+        objContainer: object,
+        srcContainer: object
+      ) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (isObject(srcValue) && srcValue[Marker.instance.get()]) {
+          return srcValue;
+        }
+
+        if (
+          srcContainer.hasOwnProperty(oneLevelPath) &&
+          srcValue === undefined
+        ) {
+          set(objContainer, [oneLevelPath], undefined);
+        }
       }
-    });
+    );
   }
 
   public static mergeIterator<T>(
