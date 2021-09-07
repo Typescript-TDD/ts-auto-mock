e84129888ba8d8f53c9042e643cd65263a485025
Vittorio Guerriero <vittorio.gue@gmail.com>
2021-09-07 07:31:04 +0100

    feat(arrowfunctionliteraltypes): make sure core ts is used at runtime

--- src/transformer/descriptor/typeNode/convertNodeToTypeNode.ts
+++ src/transformer/descriptor/typeNode/convertNodeToTypeNode.ts
@@ -31,20 +31,19 @@ export function convertNodeToTypeNode(
 
 const canTypeBeChecked: (type: ts.TypeNode) => boolean = (
   type: ts.TypeNode
-): boolean => typesThatCanBeAnalyzed.includes(type.kind);
-
-const typesThatCanBeAnalyzed: ts.SyntaxKind[] = [
-  core.ts.SyntaxKind.BigIntKeyword,
-  core.ts.SyntaxKind.NumberKeyword,
-  core.ts.SyntaxKind.StringKeyword,
-  core.ts.SyntaxKind.BooleanKeyword,
-  core.ts.SyntaxKind.TrueKeyword,
-  core.ts.SyntaxKind.FalseKeyword,
-  core.ts.SyntaxKind.ArrayType,
-  core.ts.SyntaxKind.UndefinedKeyword,
-  core.ts.SyntaxKind.AnyKeyword,
-  core.ts.SyntaxKind.NeverKeyword,
-  core.ts.SyntaxKind.UnknownKeyword,
-  core.ts.SyntaxKind.VoidKeyword,
-  core.ts.SyntaxKind.NullKeyword,
-];
+): boolean =>
+  [
+    core.ts.SyntaxKind.BigIntKeyword,
+    core.ts.SyntaxKind.NumberKeyword,
+    core.ts.SyntaxKind.StringKeyword,
+    core.ts.SyntaxKind.BooleanKeyword,
+    core.ts.SyntaxKind.TrueKeyword,
+    core.ts.SyntaxKind.FalseKeyword,
+    core.ts.SyntaxKind.ArrayType,
+    core.ts.SyntaxKind.UndefinedKeyword,
+    core.ts.SyntaxKind.AnyKeyword,
+    core.ts.SyntaxKind.NeverKeyword,
+    core.ts.SyntaxKind.UnknownKeyword,
+    core.ts.SyntaxKind.VoidKeyword,
+    core.ts.SyntaxKind.NullKeyword,
+  ].includes(type.kind);
